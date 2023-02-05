09869f1bde520c0087e7142493bb5b8b43cb8290
Ruslan Vasylyshyn <r.y.vasylyshyn@gmail.com>
2023-02-05 20:28:18 +0200

    added tablet and mobile version of hero section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,7 @@
-const burger = document.querySelector("#burger");
-burger.addEventListener("click", () => {
-  burger.classList.toggle("hero-section__burger-active");
+const burger = document.querySelector('#burger');
+const mobileNav = document.querySelector('#mobile-nav');
+
+burger.addEventListener('click', () => {
+  burger.classList.toggle('hero-section__burger-active');
+  mobileNav.classList.toggle('hero-section__navigation-mobile-active');
 });
