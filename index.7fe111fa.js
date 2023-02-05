34b973a4dc344bc0dd08713cdaa1e9d107a065d3
Ruslan Vasylyshyn <r.y.vasylyshyn@gmail.com>
2023-02-05 20:29:13 +0200

    Updates

---
+++ index.7fe111fa.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("#burger"),t=document.querySelector("#mobile-nav");e.addEventListener("click",(()=>{e.classList.toggle("hero-section__burger-active"),t.classList.toggle("hero-section__navigation-mobile-active")}));
+//# sourceMappingURL=index.7fe111fa.js.map
