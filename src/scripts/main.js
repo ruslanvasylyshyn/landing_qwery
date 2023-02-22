c797b9523a6147b081a9469f92ca571064fe0936
Ruslan Vasylyshyn <r.y.vasylyshyn@gmail.com>
2023-02-22 23:18:59 +0200

    add slider to sub section-2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,3 +5,19 @@ burger.addEventListener('click', () => {
   burger.classList.toggle('hero-section__burger-active');
   mobileNav.classList.toggle('hero-section__navigation-mobile-active');
 });
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
